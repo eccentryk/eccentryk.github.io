21ff9d00070cf799515f3a74e663518c37c71037
eccentryk <107384694+eccentryk@users.noreply.github.com>
2022-12-25 17:39:29 -0800

    adding some initial js

--- rimworld-gene-calc/js/app.js
+++ rimworld-gene-calc/js/app.js
@@ -1,1 +1,7 @@
+function init() {
+    var script = document.createElement('script');
+    script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
 
+    $( "body" ).append( "<p>Hello, testing js</p>" );
+}
