2764efc0a7577db425abf0b883f8da0801028d8d
eccentryk <107384694+eccentryk@users.noreply.github.com>
2022-12-25 22:53:42 -0800

    testing json

--- rimworld-gene-calc/js/app.js
+++ rimworld-gene-calc/js/app.js
@@ -1,7 +1,18 @@
+
+function loadGenes() {
+    $.getJSON( "data/genes.json", function( data ) {
+		var items = [];
+		var test = "<p>Hello, testing js, name: " + data[0].name + "</p>";
+		$( "body" ).append( test );
+	});
+}
+
 function init() {
     var script = document.createElement('script');
-    script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
-    document.getElementsByTagName('head')[0].appendChild(script);
-
-    $( "body" ).append( "<p>Hello, testing js</p>" );
+	document.head.appendChild(script); 
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+	script.onload = function(){
+		//$( "body" ).append( "<p>Hello, testing js</p>" );
+		loadGenes();
+	}
 }
